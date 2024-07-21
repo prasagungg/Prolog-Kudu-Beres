58cc787a60aea6ceac8bcd38dcc3b5e2e1e897f1
prasagungg <agungprasetio878@gmail.com>
2024-07-21 12:29:55 +0700

    fix: tailwind

---
+++ public/js/tailwind.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+  theme: {
+    fontFamily: {
+      sans: ['"Roboto Mono"'],
+    },
+    extend: {
+      colors: {
+        background: "#ECE6C2",
+        primary: "#73BDA8",
+        secondary: "#D2A24C",
+        coklatMuda: "#CC6B49",
+        coklatTua: "#6F5643",
+      },
+    },
+  },
+};
